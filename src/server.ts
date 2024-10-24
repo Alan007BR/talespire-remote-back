6375c8be9a231540d045794a070982dd0a40b8b7
Alan007BR <alanoliveira123bit@gmail.com>
2024-10-24 10:11:21 -0300

    feat: Add package.json, log.txt, server.ts, .gitignore, and server.js
    This commit adds the following files:
    - package.json: Contains the project's metadata, dependencies, and scripts.
    - log.txt: A log file that records requests made to the server.
    - server.ts: The main server file that handles incoming requests and sends them to Talespire.
    - .gitignore: Specifies which files and directories should be ignored by Git.
    - server.js: A compiled version of server.ts for deployment.
    
    These changes are necessary to set up the project and implement the server functionality.

---
+++ src/server.ts
@@ -0,0 +1,70 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import * as net from 'net';
+import * as fs from 'fs';
+
+
+// Configurações do servidor Express
+const app = express();
+app.use(bodyParser.text({ type: '*/*' }));
+
+const PORT = process.env.PORT || 11000; // Defina a porta do servidor
+
+// Função para enviar mensagem via TCP/IP para o Talespire
+const sendToTaleSpire = (message: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const client = new net.Socket();
+        const taleSpireHost = '127.0.0.1';
+        const taleSpirePort = 11000;
+
+        client.connect(taleSpirePort, taleSpireHost, () => {
+            client.write(message + '\n');
+        });
+
+        client.on('data', (data) => {
+            client.destroy(); // Encerrar a conexão após o recebimento dos dados
+            resolve(data.toString());
+        });
+
+        client.on('error', (error) => {
+            reject(`Failed to send ${message}: ${error.message}`);
+        });
+
+        client.on('close', () => {
+            resolve(`Message "${message}" sent successfully.`);
+        });
+    });
+};
+
+// Função para logar a mensagem
+const logMessage = (message: string) => {
+    console.log(message);
+    const logEntry = `${new Date().toISOString()} - Request: ${message}\n`;
+    fs.appendFile('log.txt', logEntry, (err) => {
+        if (err) {
+            console.error('Error writing to log file:', err);
+        }
+    });
+};
+
+// Endpoint POST para receber a mensagem
+app.post('/', async (req: Request, res: Response) => {
+    console.log(req)
+    const msg = req.body;
+    console.log(msg)
+    // Log da mensagem
+    logMessage(msg);
+
+    // Envia a mensagem para o Talespire via TCP/IP
+    try {
+        const responseMessage = await sendToTaleSpire(msg);
+        res.send(responseMessage);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
